40982683a30a56bd3f79bcc51baff4edeb50e4a2
jtquangduy <justinquangduy@gmail.com>
2022-11-30 10:29:57 +0700

    SingletonPattern

---
+++ SingletonPattern/DuySingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class DuySingleton
+    {
+        private int index;
+        private static readonly object lockObject = new object();
+        private static volatile DuySingleton uniqueInstance;
+
+        private DuySingleton(int index)
+        {
+            this.index = index;
+        }
+
+        public static DuySingleton GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (lockObject)
+                {
+                    if (uniqueInstance == null)
+                    {
+                        var random = new Random();
+                        uniqueInstance = new DuySingleton(random.Next(1, 4));
+                    }
+                }
+            }
+            return uniqueInstance;
+        }
+
+        public void SaysHi()
+        {
+            Console.WriteLine("Hello every, I am Duy namber" + index);
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var thread1 = new Thread(() => DuySingleton.GetInstance().SaysHi());
+            var thread2 = new Thread(() => DuySingleton.GetInstance().SaysHi());
+
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
